6fbdf036c23fbd4131e0021dc16b4ff86d6a70fd
ikercb2000 <ikercb2000@gmail.com>
2025-01-05 18:43:40 +0100

    add econml estimators

--- StatLearn_Projects/CausalLearning/src/classes.py
+++ StatLearn_Projects/CausalLearning/src/classes.py
@@ -1,4 +1,3 @@
-
 # Project modules
 
 from interfaces import *
@@ -7,34 +6,104 @@ from interfaces import *
 
 from dowhy import CausalModel
 from dowhy.causal_estimator import CausalEstimate
+from econml.dml import LinearDML, CausalForestDML
+from econml.dr import DRLearner
 import pandas as pd
+
+# ML Model Classes
+
+
+class LinearDMLEstimator(IEconMLEstimator):
+
+    def get_params(self):
+
+        method_params = {}
+        method_params["init_params"] = {
+            "model_y": "auto",
+            "model_t": "auto",
+        }
+        method_params["econml_estimator"] = LinearDML
+
+        return method_params
+
+
+class CausalForestDMLEstimator(IEconMLEstimator):
+
+    def get_params(self):
+
+        method_params = {}
+        method_params["init_params"] = {}
+        method_params["econml_estimator"] = CausalForestDML
+
+        return method_params
+
+
+class OrthoForestEstimator(IEconMLEstimator):
+
+    def get_params(self):
+
+        method_params = {}
+        method_params["init_params"] = {
+            "model_y": "auto",
+            "model_t": "auto",
+        }
+        method_params["econml_estimator"] = LinearDML
+
+        return method_params
+
+
+class DRLearnerEstimator(IEconMLEstimator):
+
+    def get_params(self):
+
+        method_params = {}
+        method_params["init_params"] = {}
+        method_params["econml_estimator"] = DRLearner
+
+        return method_params
+
 
 # Model Builder Class
 
+
 class CausalModelBuilder(ICausalModelBuilder):
 
-    def __init__(self, data: pd.DataFrame, treatment: str, outcome: str, common_causes: list = None):
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        treatment: str,
+        outcome: str,
+        common_causes: list = None,
+    ):
         self.data = data
         self.treatment = treatment
         self.outcome = outcome
         self.common_causes = common_causes
         self.identif_estimand = None
-    
+
     def builder(self):
-        model = CausalModel(data=self.data,
-                                 treatment=self.treatment,
-                                 outcome=self.outcome,
-                                 common_causes=self.common_causes)
-        
+        model = CausalModel(
+            data=self.data,
+            treatment=self.treatment,
+            outcome=self.outcome,
+            common_causes=self.common_causes,
+        )
+
         return model
-    
+
     def estimator(self, model: CausalModel, method: str = "backdoor.linear_regression"):
         self.identif_estimand = model.identify_effect()
-        estimate = model.estimate_effect(identified_estimand=self.identif_estimand,
-                                              method_name=method)
+        estimate = model.estimate_effect(
+            identified_estimand=self.identif_estimand, method_name=method
+        )
         return estimate
-    
-    def refuter(self, model: CausalModel, estimate: CausalEstimate, method: str = "placebo_treatment_refuter"):
+
+    def refuter(
+        self,
+        model: CausalModel,
+        estimate: CausalEstimate,
+        method: str = "placebo_treatment_refuter",
+    ):
         if not self.identif_estimand:
             raise ValueError("Target estimand is not set. Run the estimator first.")
         refutation = model.refute_estimate(
@@ -44,12 +113,22 @@ class CausalModelBuilder(ICausalModelBuilder):
         )
 
         return refutation
-    
+
+
 # Causal Pipeline Class
+
 
 class CausalPipeline(ICausalPipeline):
 
-    def __init__(self, data: pd.DataFrame, treatment: str, outcome: str, effect_method: str = "backdoor.linear_regression", refuter_method: str = "placebo_treatment_refuter", common_causes: list = None):
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        treatment: str,
+        outcome: str,
+        effect_method: str = "backdoor.linear_regression",
+        refuter_method: str = "placebo_treatment_refuter",
+        common_causes: list = None,
+    ):
 
         self.data = data
         self.treatment = treatment
@@ -60,15 +139,21 @@ class CausalPipeline(ICausalPipeline):
 
     def run(self):
         print("Constructing causal model...\n")
-        model_pipeline = CausalModelBuilder(self.data, self.treatment, self.outcome, self.common_causes)
+        model_pipeline = CausalModelBuilder(
+            self.data, self.treatment, self.outcome, self.common_causes
+        )
         model = model_pipeline.builder()
 
         print("Estimating the causal effects...\n")
         estimate = model_pipeline.estimator(model=model, method=self.effect_method)
-        print(f"Estimation Results\n------------------\n Estimated effect: {estimate.value}\n")
+        print(
+            f"Estimation Results\n------------------\n Estimated effect: {estimate.value}\n"
+        )
 
         print("Refuting estimation...\n")
-        refutation = model_pipeline.refuter(model=model, estimate=estimate, method=self.refuter_method)
+        refutation = model_pipeline.refuter(
+            model=model, estimate=estimate, method=self.refuter_method
+        )
         print(f"Refutation Results\n------------------\n{refutation}")
 
         return estimate, refutation
--- StatLearn_Projects/CausalLearning/src/interfaces.py
+++ StatLearn_Projects/CausalLearning/src/interfaces.py
@@ -4,25 +4,28 @@ from abc import ABC, abstractmethod
 
 # Model Builder Interface
 
+
 class ICausalModelBuilder(ABC):
-    
+
     @abstractmethod
     def __init__(self, data, **kwargs):
         pass
-    
+
     @abstractmethod
     def builder(self, **kwargs):
         pass
-    
+
     @abstractmethod
     def estimator(self, **kwargs):
         pass
-    
+
     @abstractmethod
     def refuter(self, **kwargs):
         pass
-    
+
+
 # Causal Pipeline Interface
+
 
 class ICausalPipeline(ABC):
 
@@ -34,3 +37,20 @@ class ICausalPipeline(ABC):
     def run(self):
         pass
 
+
+# Causal Estimator Interface
+
+
+class IEconMLEstimator(ABC):
+
+    @abstractmethod
+    def __init__(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def get_params(self, **kwargs):
+        pass
+
+
+# TODO: Integrate sklearn models and other features for optimization and validation.
+# See https://econml.azurewebsites.net/spec/estimation.html for examples
