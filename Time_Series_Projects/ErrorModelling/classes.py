0865e073f11d2b6d6751827ffaf43b85ba370767
ikercb2000 <ikercb2000@gmail.com>
2025-01-13 14:33:49 +0100

    include shifting distributions

--- Time_Series_Projects/ErrorModelling/classes.py
+++ Time_Series_Projects/ErrorModelling/classes.py
@@ -18,11 +18,14 @@ class RecoveryPlotHist(IPlot):
 
         self.errors = errors
         self.dist = dist
-        if dist != None:
-            self.x_pdf = np.linspace(errors.min(), errors.max(), 1000)
+        if dist is not None:
+            self.x_pdf = np.linspace(errors.min()*4, errors.max()*4, 1000)
             self.pdf = dist.pdf(self.x_pdf)
 
-    def get_plot(self, loss_name: str, plot_stats: bool = True):
+            if dist.demean:
+                self.x_pdf = np.linspace(errors.min()*4-dist.theory()["mean"], errors.max()*4-dist.theory()["mean"], 1000)
+
+    def get_plot(self, loss_name: str, x_limits: list = None, y_limits: list = None, plot_stats: bool = True):
 
         plt.figure(figsize=(10, 6))
         plt.hist(self.errors, bins=25, edgecolor='k', alpha=0.7, density=True)
@@ -32,7 +35,7 @@ class RecoveryPlotHist(IPlot):
             plt.axvline(np.median(self.errors), color='green', linestyle='-.', label=f"Median: {np.median(self.errors):.2f}")
         
         if self.dist != None:
-            plt.plot(self.x_pdf, self.pdf, color='purple', linestyle='-', linewidth=2, label=f"{self.dist.__str__(for_title=True)} PDF")
+            plt.plot(self.x_pdf, self.pdf, color='purple', linestyle='-', linewidth=2, label=f"{self.dist.__str__(for_title=True)} PDF")   
         
         plt.xlabel("Error (Real Value - Prediction)")
         plt.ylabel("Frequency")
@@ -41,6 +44,11 @@ class RecoveryPlotHist(IPlot):
             plt.title(f"Histogram of Errors for a {loss_name} loss (No noise)")
         else:
             plt.title(f"Histogram of Errors for a {self.dist.__str__(for_title=True)}-distributed error and a {loss_name} loss")
+
+        if x_limits is not None:
+            plt.xlim(x_limits)
+        if y_limits is not None:
+            plt.ylim(y_limits)
         
         plt.grid(axis='y', linestyle='--', alpha=0.7)
         plt.legend()
@@ -52,9 +60,12 @@ class RecoveryPlotKernel(IPlot):
         
         self.errors = errors
         self.dist = dist
-        if dist != None:
-            self.x_pdf = np.linspace(errors.min(), errors.max(), 1000)
+        if dist is not None:
+            self.x_pdf = np.linspace(errors.min()*4, errors.max()*4, 1000)
             self.pdf = dist.pdf(self.x_pdf)
+
+            if dist.demean:
+                self.x_pdf = np.linspace(errors.min()*4-dist.theory()["mean"], errors.max()*4-dist.theory()["mean"], 1000)
 
         kde = KDEUnivariate(errors)
 
@@ -68,7 +79,7 @@ class RecoveryPlotKernel(IPlot):
         self.x = np.linspace(errors.min(), errors.max(), 100)
         self.y = np.array([float(kde.evaluate(val)) for val in self.x])
 
-    def get_plot(self, loss_name: str, plot_stats: bool = True):
+    def get_plot(self, loss_name: str, x_limits: list = None, y_limits: list = None, plot_stats: bool = True):
 
         plt.figure(figsize=(10, 6))
         plt.plot(self.x, self.y, label="KDE with Epanechnikov kernel")
@@ -87,6 +98,12 @@ class RecoveryPlotKernel(IPlot):
             plt.title(f"KDE of Errors for a {loss_name} loss (No noise)")
         else:
             plt.title(f"KDE of Errors for a {self.dist.__str__(for_title=True)}-distributed error and a {loss_name} loss")
-        
+
+        if x_limits is not None:
+            plt.xlim(x_limits)
+        if y_limits is not None:
+            plt.ylim(y_limits)
+
+        plt.grid(axis='y', linestyle='--', alpha=0.7)
         plt.legend()
         plt.show()
